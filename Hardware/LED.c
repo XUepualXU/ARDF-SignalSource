ab148df114a5a32a9a397f4fb8b6c5e450792074
XUepualXU <100829072+XUepualXU@users.noreply.github.com>
2024-10-23 15:26:53 +0800

    Update LED.c

--- Hardware/LED.c
+++ Hardware/LED.c
@@ -1,29 +1,35 @@
 #include "stm32f10x.h"                  // Device header
 
-/* 操作GPIO需要3个步骤
-1、使用RCC开启GPIO的时钟
-2、使用GPIOInit函数初始化GPIO
-3、使用输出或者输入的函数控制GPIO
+/*
+  * @brief  初始化LAMP与CTRL对应的引脚
+  * @param  None
+  * @retval None
 */
 void LED_Init(void){
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
-	
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 	
 	GPIO_SetBits(GPIOB, GPIO_Pin_10 | GPIO_Pin_11);
-	
 }
-
+/*
+  * @brief  将LAMP与CTRL置高电平
+  * @param  None
+  * @retval None
+*/
 void High(void){
 	GPIO_SetBits(GPIOB, GPIO_Pin_10);
 	GPIO_SetBits(GPIOB, GPIO_Pin_11);
 }
-
+/*
+  * @brief  将LAMP与CTRL置低电平
+  * @param  None
+  * @retval None
+*/
 void Low(void){
 	GPIO_ResetBits(GPIOB, GPIO_Pin_10);
 	GPIO_ResetBits(GPIOB, GPIO_Pin_11);
