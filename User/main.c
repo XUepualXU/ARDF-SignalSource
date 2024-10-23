309eaed633a2490dbc9b114bca6c1694a6f3bea7
XUepualXU <100829072+XUepualXU@users.noreply.github.com>
2024-10-23 12:15:03 +0800

    Update main.c

--- User/main.c
+++ User/main.c
@@ -26,7 +26,11 @@
 #include "LED.H"
 
 #define time  100
-//#define Choose  10
+/* 
+address[]: CDCE913中的寄存器地址
+data[]: 寄存器中应该写入的值
+CDCE_Addr: CDCE913在I2C总线上的地址（向左平移1位后的）
+*/
 uint8_t address[] = {0, 1, 2, 3, 4, 5, 6, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 uint8_t data[] = {0x81, 0x01, 0xb4, 0x41, 0x02, 0x50, 0x41, 0x00, 0x00, 0x00, 0x00, 0x6d, 0x02, 0x00, 0x00, 0xff, 0xfc, 0xf2, 0x47, 0xff, 0xfc, 0xf2, //3.50  0
                0x81, 0x01, 0xb4, 0x40, 0x02, 0x50, 0x41, 0x00, 0x00, 0x00, 0x00, 0x6d, 0x02, 0x00, 0x00, 0xf8, 0xa9, 0x92, 0x47, 0xf8, 0xa9, 0x92, //3.51  1
@@ -42,8 +46,12 @@ uint8_t data[] = {0x81, 0x01, 0xb4, 0x41, 0x02, 0x50, 0x41, 0x00, 0x00, 0x00, 0x
                0x81, 0x01, 0xb4, 0x01, 0x02, 0x50, 0x41, 0x00, 0x00, 0x00, 0x00, 0x6d, 0x02, 0x00, 0x00, 0xbf, 0xa0, 0x03, 0x09, 0xbf, 0xa0, 0x03
               };//144
 #define CDCE_Addr 0xCA
-#define RegAddress  0x06
-			  
+
+/*
+  * @brief  根据选择的台号，向寄存器写入相应频率对应的值.
+  * @param  Choose: 选择的台号. [MO台认为是10号台]
+  * @retval None
+*/
 void Send_Choose(uint8_t Choose){
 	for(int i = 2; i < 22; i ++){
 		CDCE_I2C_Start();
@@ -57,25 +65,56 @@ void Send_Choose(uint8_t Choose){
 	}
 }
 
+/*
+  * @brief  摩斯码中长信号.
+  * @param  None
+  * @retval None
+*/
 void DASH(){
 	High();
 	Delay_ms(4 * time);
 	Low();
 	Delay_ms(2 * time);
 }
+/*
+  * @brief  摩斯码中短信号.
+  * @param  None
+  * @retval None
+*/
 void DOT(){
 	High();
 	Delay_ms(1.2 * time);
 	Low();
 	Delay_ms(1.5 * time);
 }
+/*
+  * @brief  摩斯码中间的停顿，台号和台号中间停顿两次.
+  * @param  None
+  * @retval None
+*/
 void Longrest(){
 	Low();
 	Delay_ms(3 * time);
 }
 
-void speak(uint8_t way){
-	if (way == 0){
+/*
+  * @brief  实现对应台号的摩斯码.
+  * @param  Choose: 选择的台号
+  			0号台: _ _ _ _ _
+  			1号台: _ _  _ _ _ .
+     			2号台: _ _  _ _ _ . .
+	     		3号台: _ _  _ _ _ . . .
+	     		4号台: _ _  _ _ _ . . . .
+	     		5号台: _ _  _ _ _ . . . . .
+	     		6号台: _ . . . .
+	     		7号台: _ _ . . .
+	     		8号台: _ _ _ . .
+	     		9号台: _ _ _ _ .
+	     		MO 台: _ _  _ _ _ 
+  * @retval None
+*/
+void speak(uint8_t Choose){
+	if (Choose == 0){
 		DASH();
 		DASH();
 		DASH();
@@ -85,7 +124,7 @@ void speak(uint8_t way){
 		Longrest();
 	}
 
-	if (way == 1){
+	if (Choose == 1){
 		DASH();
 		DASH();
 		Longrest();
@@ -97,7 +136,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	}
-	if (way == 2){
+	if (Choose == 2){
 		DASH();
 		DASH();
 		Longrest();
@@ -110,7 +149,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	 }
-	if (way == 3){
+	if (Choose == 3){
 		DASH();
 		DASH();
 		Longrest();
@@ -124,7 +163,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	 }
-	if (way == 4){
+	if (Choose == 4){
 		DASH();
 		DASH();
 		Longrest();
@@ -139,7 +178,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	}
-	if (way == 5){
+	if (Choose == 5){
 		DASH();
 		DASH();
 		Longrest();
@@ -155,7 +194,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	}
-	if (way == 6){
+	if (Choose == 6){
 		DASH();
 		DOT();
 		DOT();
@@ -164,7 +203,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	}
-	if (way == 7){
+	if (Choose == 7){
 		DASH();
 		DASH();
 		DOT();
@@ -173,7 +212,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	}
-	if (way == 8){
+	if (Choose == 8){
 		DASH();
 		DASH();
 		DASH();
@@ -182,7 +221,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	}
-	if (way == 9){
+	if (Choose == 9){
 		DASH();
 		DASH();
 		DASH();
@@ -191,7 +230,7 @@ void speak(uint8_t way){
 		Longrest();
 		Longrest();
 	}
-	if (way == 10){
+	if (Choose == 10){
 		DASH();
 		DASH();
 		Longrest();
@@ -203,39 +242,48 @@ void speak(uint8_t way){
 	}
 }
 
+/*
+  * @brief  主函数
+  * @param  None
+  * @retval None
+*/
 int main(){
 	uint8_t Key_Num, Key_tmp, data;
-	OLED_Init();
-	CDCE_I2C_Init();
-	LED_Init();
-	Key_Init();
-	
+	OLED_Init();  // 屏幕显示，测试使用
+	//CDCE_I2C_Init();  // 初始化
+	LED_Init();  // 初始化控制LAMP和CTRL的引脚。CTRL用来控制CDCE913间断发送信号，LAMP使用led灯观察信号变化
+	Key_Init();  // 初始化与8421拨码开关相连的引脚
+
+	/*
+	如果8421拨码开关上的数字大于10，全按照10处理
+	*/
 	Key_Num = Key_GetNum();
 	if (Key_Num > 10){
 		Key_Num = 10;
 	}
+
 	Send_Choose(Key_Num);
-	OLED_ShowString (3,1,"data:");
-	OLED_ShowHexNum (3,6,data, 2);
-
-
-	OLED_ShowString (1,1,"Num:");
-	OLED_ShowString (2,1,"tmp:");
 	
-
+	//OLED_ShowString (3,1,"data:");  // 测试数据
+	//OLED_ShowHexNum (3,6,data, 2);
+	//OLED_ShowString (1,1,"Num:");
+	//OLED_ShowString (2,1,"tmp:");
 
 	while(1){
+		/* 
+  		循环读入拨码开关对应的值，如果被改变，则发送新的台号对应的信号
+    		注： 上一个信号完整发送完毕之后才会换成新的信号
+    		*/
 		Key_tmp = Key_GetNum();
 		if (Key_tmp > 10){
 			Key_tmp = 10;
 		}
-		OLED_ShowNum (1,5,Key_tmp,3);
-		OLED_ShowNum (2,5,Key_Num,3);
+		// OLED_ShowNum (1,5,Key_tmp,3);
+		// OLED_ShowNum (2,5,Key_Num,3);
 		if(Key_Num != Key_tmp){
 			Send_Choose(Key_tmp);
 			Key_Num = Key_tmp;
 		}
 		speak(Key_Num);
-		
 	}
 }
